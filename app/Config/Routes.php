9aa0751e20a677e5ab509c335c53ae8c2db1c88b
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2025-08-17 00:45:44 +0700

    refactor: add prefix auth route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,11 +10,13 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', [Home::class, 'index']);
 $routes->get('403', [Home::class, 'http403']);
 
-$routes->post('login', [AuthController::class, 'login']);
+$routes->post('auth/login', [AuthController::class, 'login']);
 
 $routes->group('', ['filter' => 'tokens'], function (RouteCollection $routes) {
-    $routes->get('me', [AuthController::class, 'me']);
-    $routes->post('logout', [AuthController::class, 'logout']);
+    $routes->group('auth', function (RouteCollection $routes) {
+        $routes->get('me', [AuthController::class, 'me']);
+        $routes->post('logout', [AuthController::class, 'logout']);
+    });
     
     $routes->get('superadmin-only', fn () => response()->setJSON(['message' => 'OK GRANTED!']), ['filter' => 'group:superadmin']);
     $routes->get('user-only', fn () => response()->setJSON(['message' => 'OK GRANTED!']), ['filter' => 'group:user']);
